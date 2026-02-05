4ec1d3baf9fd90cd25037ba3647287c8e049d03e
tvpx-dev <tvpx-dev@gmail.com>
2026-02-05 19:53:50 -0300

    Transfere o conteúdo para a página cadastrar pessoa

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { TextField } from './shared/components/formulario/text-field/text-field';
+
+const COMPONENTES_ANGULAR = [RouterOutlet, ReactiveFormsModule];
+const COMPONENTES_PRIME = [];
+const COMPONENTES_CONNECT = [];
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [...COMPONENTES_ANGULAR],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
-  protected readonly title = signal('connect');
-}
+export class App {}
