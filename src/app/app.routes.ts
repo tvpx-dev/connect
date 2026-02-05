128aa81d29d6ef7e1c770ddbc144214b64eb8a51
tvpx-dev <tvpx-dev@gmail.com>
2026-02-05 19:52:58 -0300

    criação de rota para a página cadastrar pessoas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { CadastroUnicoPessoas } from './pages/cadastrar/cadastro-unico-pessoas/cadastro-unico-pessoas';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'cadastrar/cadastro-unico-pessoas', pathMatch: 'full' },
+  { path: 'cadastrar/cadastro-unico-pessoas', component: CadastroUnicoPessoas },
+];
